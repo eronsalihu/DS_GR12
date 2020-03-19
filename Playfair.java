04ae9be19457c301e4b1f9af526918b895e4f019
eronsalihu <eronsalihu@users.noreply.github.com>
2020-03-19 23:47:30 +0100

    Update Playfair.java

--- Playfair.java
+++ Playfair.java
@@ -1,92 +1,75 @@
-
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Playfair {
-    private static String next=new String();
+    static String input;
+    static String[] inputSplited;
+    static String textToEncryptOrDecrypt;
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        boolean done1 = false;
-        boolean done2 = false;
+
+        Scanner in = new Scanner(System.in);
         KeyTable table = null;
         Phrase phraseUsed = null;
         String phrase = null;
-         next = in.nextLine();
-
-        while (!done1) {
-            String key = in.nextLine();
-            if (!key.isEmpty()) {
-                phrase = key;
-            }
-            try {
-                table = KeyTable.buildFromString(phrase);
-                done1 = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println("Illegal argument");
-            }
-        }
-        while (!done2) {
-
-
-
-            if (next.equals("ds playfair encrypt")) {
-                String encrypt =
-                        in.nextLine().replaceAll("\\s", "");
-                phraseUsed = Phrase.buildPhraseFromStringforEnc(encrypt);
-                phraseUsed = phraseUsed.encrypt(table);
-                System.out.println("Encrypted text is: " +
-                        phraseUsed.toString());
-                break;
-            }
-
-            else if (next.equals("ds playfair decrypt")) {
-                System.out.print("Please enter a phrase to decrypt: ");
-                String decrypt = in.nextLine();
-                decrypt = decrypt.toUpperCase();
-                if (decrypt.contains("J")) {
-                    System.out.println("Invalid statement to decrypt.");
-                    continue;
-                }
-                for (int i = 0; i < decrypt.length(); i++) {
-                    if (!Character.isLetter(decrypt.charAt(i))) {
-
-                        continue;
-                    }
-                }
-
-                Phrase decryptPhrase =
-                        Phrase.buildPhraseFromStringforEnc(decrypt);
-                decryptPhrase = decryptPhrase.decrypt(table);
-                System.out.println("Decrypted text is: " +
-                        decryptPhrase.toString());
-                break;
-            }
-            else {
-                System.out.println("Invalid input.");
-            }
-        }
-
+        input = in.nextLine();
+        input = input.trim();
+        inputSplited = input.split(" ");
         in.close();
 
+        if (inputSplited.length < 4) {
+            System.out.println("Illegal argument");
+            System.exit(-1);
+        }
+        phrase = inputSplited[3];
+        textToEncryptOrDecrypt = getTextFromInput();
+
+        try {
+            table = KeyTable.buildFromString(phrase);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal argument");
+        }
+        if (inputSplited[2].equals("encrypt")) {
+            textToEncryptOrDecrypt =
+                    textToEncryptOrDecrypt.replaceAll("\\s", "");
+            phraseUsed = Phrase.buildPhraseFromStringforEnc(textToEncryptOrDecrypt);
+            phraseUsed = phraseUsed.encrypt(table);
+            System.out.println(
+                    phraseUsed.toString());
+        } else if (inputSplited[2].equals("decrypt")) {
+            Phrase decryptPhrase =
+                    Phrase.buildPhraseFromStringforEnc(getTextFromInput());
+            decryptPhrase = decryptPhrase.decrypt(table);
+            System.out.println(
+                    decryptPhrase.toString());
+            if (getTextFromInput().contains("J")) {
+                System.out.println("Invalid statement to decrypt. Cannot count letter J");
+                System.exit(-1);
+            }
+            for (int i = 0; i < getTextFromInput().length(); i++) {
+                if (!Character.isLetter(getTextFromInput().charAt(i))) {
+                    continue;
+                } else {
+                    System.exit(-1);
+                }
+            }
+        } else {
+            System.out.println("Invalid input.");
+        }
     }
 
-
+    private static String getTextFromInput() {
+        String text = "";
+        for (int i = 4; i < inputSplited.length; i++) {
+            text += inputSplited[i];
+        }
+        return text;
+    }
 }
+
+
 class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
-
-    public void enqueue(Bigram b) {
-        this.addLast(b);
-    }
-
-    public Bigram dequeue() {
-        return (Bigram) this.remove();
-    }
-
-    public Bigram peek() {
-        return (Bigram) this.getFirst();
-    }
 
     public static Phrase buildPhraseFromStringforEnc(String s) {
         s = s.toUpperCase();
@@ -103,12 +86,10 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
             if (i == s2.length() - 1) {
                 b = new Bigram(s2.charAt(i), 'X');
                 i++;
-            }
-            else if (s2.charAt(i) == s2.charAt(i + 1)) {
+            } else if (s2.charAt(i) == s2.charAt(i + 1)) {
                 b = new Bigram(s2.charAt(i), 'X');
                 i++;
-            }
-            else {
+            } else {
                 b = new Bigram(s2.charAt(i), s2.charAt(i + 1));
                 i = i + 2;
             }
@@ -117,14 +98,12 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
         return p;
     }
 
-
     public static char findEncryptedRight(KeyTable k, char c) {
         int i = k.findRow(c);
         int j = k.findCol(c);
         if (j == k.getKeyTable()[0].length - 1) {
             j = 0;
-        }
-        else {
+        } else {
             j++;
         }
         return k.getKeyTable()[i][j];
@@ -139,19 +118,16 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
         int i = k.findRow(c);
         if (i == k.getKeyTable().length - 1) {
             i = 0;
-        }
-        else {
+        } else {
             i++;
         }
         return k.getKeyTable()[i][k.findCol(c)];
     }
 
-
     public static void encryptBottomShift(KeyTable k, Bigram b) {
         b.setFirst(findEncryptedBottom(k, b.getFirstChar()));
         b.setSecond(findEncryptedBottom(k, b.getSecondChar()));
     }
-
 
     public static void encryptRectangle(KeyTable k, Bigram b) {
         int i1 = k.findRow(b.getFirstChar());
@@ -165,54 +141,22 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
         b.setSecond(k.getKeyTable()[i2][j2]);
     }
 
-    public Phrase encrypt(KeyTable key) {
-        if (key == null) {
-            throw new IllegalArgumentException();
-        }
-        Phrase encrypted = new Phrase();
-        while (!this.isEmpty()) {
-            Bigram b = this.dequeue();
-            if (key.findRow(b.getFirstChar()) ==
-                    key.findRow(b.getSecondChar())) {
-                encryptRightShift(key, b);
-            }
-            else if (key.findCol(b.getFirstChar()) ==
-                    key.findCol(b.getSecondChar())) {
-                encryptBottomShift(key, b);
-            }
-            else {
-                encryptRectangle(key, b);
-            }
-            encrypted.enqueue(b);
-        }
-        return encrypted;
-    }
-
     public static char findDecryptedLeft(KeyTable k, char c) {
         int i = k.findRow(c);
         int j = k.findCol(c);
         if (j == 0) {
             j = k.getKeyTable()[0].length - 1;
-        }
-        else {
+        } else {
             j--;
         }
         return k.getKeyTable()[i][j];
     }
 
-
-    public void decryptLeftShift(KeyTable k, Bigram b) {
-        b.setFirst(findDecryptedLeft(k, b.getFirstChar()));
-        b.setSecond(findDecryptedLeft(k, b.getSecondChar()));
-    }
-
-
     public static char findDecryptedTop(KeyTable k, char c) {
         int i = k.findRow(c);
         if (i == 0) {
             i = k.getKeyTable().length - 1;
-        }
-        else {
+        } else {
             i--;
         }
         return k.getKeyTable()[i][k.findCol(c)];
@@ -223,6 +167,44 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
         b.setSecond(findDecryptedTop(k, b.getSecondChar()));
     }
 
+    public void enqueue(Bigram b) {
+        this.addLast(b);
+    }
+
+    public Bigram dequeue() {
+        return (Bigram) this.remove();
+    }
+
+    public Bigram peek() {
+        return (Bigram) this.getFirst();
+    }
+
+    public Phrase encrypt(KeyTable key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        Phrase encrypted = new Phrase();
+        while (!this.isEmpty()) {
+            Bigram b = this.dequeue();
+            if (key.findRow(b.getFirstChar()) ==
+                    key.findRow(b.getSecondChar())) {
+                encryptRightShift(key, b);
+            } else if (key.findCol(b.getFirstChar()) ==
+                    key.findCol(b.getSecondChar())) {
+                encryptBottomShift(key, b);
+            } else {
+                encryptRectangle(key, b);
+            }
+            encrypted.enqueue(b);
+        }
+        return encrypted;
+    }
+
+    public void decryptLeftShift(KeyTable k, Bigram b) {
+        b.setFirst(findDecryptedLeft(k, b.getFirstChar()));
+        b.setSecond(findDecryptedLeft(k, b.getSecondChar()));
+    }
+
     public Phrase decrypt(KeyTable key) {
         if (key == null) {
             throw new IllegalArgumentException();
@@ -231,14 +213,12 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
         while (!this.isEmpty()) {
             Bigram b = this.dequeue();
             if (key.findRow(b.getFirstChar()) != key.findRow(b.getSecondChar())
-                    && key.findCol(b.getFirstChar()) != key.findCol(b.getSecondChar())){
+                    && key.findCol(b.getFirstChar()) != key.findCol(b.getSecondChar())) {
                 encryptRectangle(key, b);
-            }
-            else if (key.findCol(b.getFirstChar()) ==
-                    key.findCol(b.getSecondChar())){
+            } else if (key.findCol(b.getFirstChar()) ==
+                    key.findCol(b.getSecondChar())) {
                 decryptTopShift(key, b);
-            }
-            else {
+            } else {
                 decryptLeftShift(key, b);
             }
             decrypted.enqueue(b);
@@ -261,11 +241,12 @@ class Phrase extends LinkedList<Bigram> implements Queue<Bigram> {
     }
 
 }
+
 class KeyTable {
     private static final int MAX_ROWS = 5;
     private static final int MAX_COLUMNS = 5;
+    Playfair playfair = new Playfair();
     private char[][] key = new char[MAX_ROWS][MAX_COLUMNS];
-    Playfair playfair=new Playfair();
 
 
     public KeyTable() {
@@ -296,9 +277,7 @@ class KeyTable {
         for (int i = 1; i <= 26; i++) {
             if (i == 10) {
                 continue;
-            }
-            
-            else {
+            } else {
                 if ((!newString2.contains("" + ((char) (i + 64)))) && i != 10) {
                     newString2 += ((char) (i + 64));
                 }
@@ -359,6 +338,7 @@ class KeyTable {
     }
 
 }
+
 class Bigram {
     private char first, second;
 
@@ -394,4 +374,3 @@ class Bigram {
         second = c;
     }
 }
-
