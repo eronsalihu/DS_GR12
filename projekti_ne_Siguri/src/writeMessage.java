9e6124edbcbc1068f8b645dbfc187ceafee8f7fd
DonikaRexhepi <58113281+DonikaRexhepi@users.noreply.github.com>
2020-05-03 00:15:20 +0200

    Update writeMessage.java

--- projekti_ne_Siguri/src/writeMessage.java
+++ projekti_ne_Siguri/src/writeMessage.java
@@ -88,6 +88,88 @@ public class writeMessage {
         }
     }
     public static void write_Message(String outFile, String message, String file) {
+try {
+            File pubfile = new File("C:\\Users\\Lenovo\\IdeaProjects\\exportKey\\src\\test\\keys\\" + outFile + ".pub.xml"); //Path to xml file
+            if (pubfile.exists()) {
+                byte[] bytes = outFile.getBytes("UTF-8");
+                String emri = Base64.getEncoder().encodeToString(bytes);
 
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(pubfile);
+                doc.getDocumentElement().normalize();
+
+                //gjenerimi i 8 bytes per iv
+                SecureRandom sr = new SecureRandom(); //create new secure random
+                byte[] iv = new byte[8]; //create an array of 8 bytes
+                sr.nextBytes(iv);
+
+                String ivi = Base64.getEncoder().encodeToString(iv);
+
+                //gjenerimi i 8 bytes per qelesin key dhe konvertimi ne qeles sekret
+                Random keyi = new Random();
+                byte[] keys = new byte[8];
+                keyi.nextBytes(keys);
+                KeySpec myKeySpec = new DESKeySpec(keys);
+                SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+
+                SecretKey celesi = secretKeyFactory.generateSecret(myKeySpec);
+
+
+                //Fitimi i qelesit publik nga XML dokumenti
+                String moduliS = ((org.w3c.dom.Document) doc).getElementsByTagName("Modulus").item(0).getTextContent();
+                String exponentAsString = ((org.w3c.dom.Document) doc).getElementsByTagName("Exponent").item(0).getTextContent();
+
+                BigInteger modulus = new BigInteger(Base64.getDecoder().decode(moduliS));
+                BigInteger exponent = new BigInteger(Base64.getDecoder().decode(exponentAsString));
+
+                //formimi i RSA publci key nga file ne xml
+                RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                PublicKey pubKey = keyFactory.generatePublic(keySpec);
+
+                //enkriptimi i rsa key me key
+                Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+                byte[] encryptedKe = cipher.doFinal(celesi.getEncoded());
+                String encryptedKey = Base64.getEncoder().encodeToString(encryptedKe);
+
+                //enkriptimi i mesazhit me key
+                Cipher encryptedcipher = Cipher.getInstance("DES");
+                encryptedcipher.init(Cipher.ENCRYPT_MODE, celesi);
+                byte[] utf8 = message.getBytes("UTF8");
+                byte[] enc = encryptedcipher.doFinal(utf8);
+                enc = Base64.getEncoder().encode(enc);
+                String encryptedWord = Base64.getEncoder().encodeToString(enc);
+
+
+              //  System.out.println(emri + "." + ivi + "." + encryptedKey + "." + encryptedWord);
+
+                FileOutputStream outStream = new FileOutputStream(new File(String.valueOf(file)));
+                String str=emri + "." + ivi + "." + encryptedKey + "." + encryptedWord;
+                byte[] strToByte=str.getBytes();
+                outStream.write(strToByte);
+            }
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
