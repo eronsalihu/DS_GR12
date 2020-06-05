a94ca447c8133a708521ddb0647cc22b2d65c063
Enis Berisha <58141985+enisbe1@users.noreply.github.com>
2020-06-05 13:49:44 +0200

    Update logIn.java

--- projekti_ne_Siguri/src/logIn.java
+++ projekti_ne_Siguri/src/logIn.java
@@ -20,27 +20,23 @@ import java.util.Date;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class logIn {
-    public static void main(String[] args) throws Exception {
-      //  login("donika");
 
-       statusi("eydhbGcnOidSUzIwNDgnLCd0eXAnOidKV1QnfQ==.eyduYW1lJzonZG9uaWthJywnZXhwaXJhdGlvbl9kYXRlJzonMDQvMDYvMjAyMCAxNDozNSd9.BHg9LitRp2c+PTFS6xac5ees/5xA2iX09NnCtt0dvYZ1SgrJ3xpiOMBDnpkJqq918lsYtvnfw5LyI66UMxB9gkQhEkXD72KgInpGHJiojCLMbYbcllVJFwg14HRKytlTq4BpG3vZR6jhhbBiaCp56EdqeQUCf9vN9evlmrjNeNmsgiNzHXYuc95hnht2q+1IzIGQaLSg4/ldFkdz9Z0utn3hAQxgFSKaUQI9oG+vyBfxuu282Uox2FP5ySBAi815PWOswwUlYdxxjYk8tU+4NSaBrMTOiTjv3boEPzMCYa25P0hObirgRSUrlh+mMash3z+Z/mu05O1CXIb3j1Esvg==");
-    }
     public static void login(String name) throws Exception {
 
         System.out.println("Token: " + sign(name));
-        boolean a=verify(sign(name));
-       if (a)
+        boolean a = verify(sign(name));
+        if (a)
             System.out.println("Mir");
 
-        File tokens=new File("tokens/"+name+".txt");
-        FileOutputStream fileOutputStream=new FileOutputStream(new File(String.valueOf(tokens)));
-        byte [] bytes=sign(name).getBytes();
+        File tokens = new File("tokens/" + name + ".txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(String.valueOf(tokens)));
+        byte[] bytes = sign(name).getBytes();
         fileOutputStream.write(bytes);
 
     }
     public static String sign(String name) throws Exception {
 
-        File file=new File("keys/"+name+".xml");
+        File file = new File("keys/" + name + ".xml");
 
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -58,17 +54,17 @@ public class logIn {
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
 
-        long currentTimeMillis = System.currentTimeMillis()+ ( 20 * 60 * 1000);
+        long currentTimeMillis = System.currentTimeMillis() + (20 * 60 * 1000);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         Date resultdate = new Date(currentTimeMillis);
-        String string=sdf.format(resultdate);
-        String algoritmi="{'alg':'RS2048'," +
-                "'typ':'JWT'" +
-                "}";
-        String tedhenat="{'name':'"+name+"'," +
-                "'expiration_date':'"+string+"'}";
-        String algoritmi_b64=Base64.getEncoder().encodeToString(algoritmi.getBytes());
-        String tedhenat_b64=Base64.getEncoder().encodeToString(tedhenat.getBytes());
+        String string = sdf.format(resultdate);
+        String algoritmi = "{'alg':'RS2048'," +
+            "'typ':'JWT'" +
+            "}";
+        String tedhenat = "{'name':'" + name + "'," +
+            "'expiration_date':'" + string + "'}";
+        String algoritmi_b64 = Base64.getEncoder().encodeToString(algoritmi.getBytes());
+        String tedhenat_b64 = Base64.getEncoder().encodeToString(tedhenat.getBytes());
 
 
 
@@ -79,8 +75,8 @@ public class logIn {
 
         byte[] signature = privateSignature.sign();
 
-        String signi= Base64.getEncoder().encodeToString(signature);
-        String komplet=algoritmi_b64+"."+tedhenat_b64+"."+signi;
+        String signi = Base64.getEncoder().encodeToString(signature);
+        String komplet = algoritmi_b64 + "." + tedhenat_b64 + "." + signi;
         return komplet;
 
 
@@ -88,16 +84,16 @@ public class logIn {
 
     public static boolean verify(String signature) throws Exception {
         String[] stringss = signature.split("\\.");
-        ArrayList<String> aListNumberss = new ArrayList<String>(Arrays.asList(stringss));
+        ArrayList < String > aListNumberss = new ArrayList < String > (Arrays.asList(stringss));
         byte[] tedhenat = Base64.getDecoder().decode(aListNumberss.get(1));
-        String data=new String(tedhenat);
-        String nenshkrimi=signature.split("\\.")[2];
+        String data = new String(tedhenat);
+        String nenshkrimi = signature.split("\\.")[2];
 
-        String name=data.split("'")[3];
+        String name = data.split("'")[3];
 
 
 
-        File file=new File("keys/"+name+".pub.xml");
+        File file = new File("keys/" + name + ".pub.xml");
 
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -123,35 +119,63 @@ public class logIn {
 
         return publicSignature.verify(signatureBytes);
     }
+    //verifiko me fjalen
+    public static boolean verifikoFjala(String e_dhena, String name, String nenshkrimi) throws Exception {
+
+        File file = new File("keys/" + name + ".pub.xml");
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(file);
+        doc.getDocumentElement().normalize();
+
+        String moduliS = doc.getElementsByTagName("Modulus").item(0).getTextContent();
+        String exponentAsString = doc.getElementsByTagName("Exponent").item(0).getTextContent();
+
+        BigInteger modulus = new BigInteger(Base64.getDecoder().decode(moduliS));
+        BigInteger exponent = new BigInteger(Base64.getDecoder().decode(exponentAsString));
+
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+
+        Signature publicSignature = Signature.getInstance("SHA1withRSA");
+        publicSignature.initVerify(publicKey);
+        publicSignature.update((e_dhena).getBytes(UTF_8));
+
+        byte[] signatureBytes = Base64.getDecoder().decode(nenshkrimi);
+
+        return publicSignature.verify(signatureBytes);
+
+    }
+
     public static void statusi(String signature) throws Exception {
-        boolean nenshkrim_valid=verify(signature);
-        if (nenshkrim_valid){
+        boolean nenshkrim_valid = verify(signature);
+        if (nenshkrim_valid) {
             String[] stringss = signature.split("\\.");
-            ArrayList<String> aListNumberss = new ArrayList<String>(Arrays.asList(stringss));
+            ArrayList < String > aListNumberss = new ArrayList < String > (Arrays.asList(stringss));
             byte[] tedhenat = Base64.getDecoder().decode(aListNumberss.get(1));
-            String data=new String(tedhenat);
-            String name=data.split("'")[3];
-            String dataSkadimit=data.split("'")[7];
+            String data = new String(tedhenat);
+            String name = data.split("'")[3];
+            String dataSkadimit = data.split("'")[7];
             long currentTimeMillis = System.currentTimeMillis();
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
             Date resultdate = new Date(currentTimeMillis);
-            String string=sdf.format(resultdate);
+            String string = sdf.format(resultdate);
 
 
-            int compareTO=string.compareTo(dataSkadimit);
-            if (compareTO<0)
-            {
+            int compareTO = string.compareTo(dataSkadimit);
+            if (compareTO < 0) {
                 lidhjameDB.gjeje(name);
                 System.out.println("Valid: Po");
-                System.out.println("Skadimi: "+dataSkadimit);
+                System.out.println("Skadimi: " + dataSkadimit);
 
-            }
-            else System.out.println("Jo Valid");
-
+            } else System.out.println("Jo Valid");
 
 
-        }
-        else System.out.println("Jo Valid");
+
+        } else System.out.println("Jo Valid");
     }
 
 }
